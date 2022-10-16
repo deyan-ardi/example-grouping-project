47c395c9811062432663e533adcecf0e39aec064
deyan-ardi <riyan.clsg11@gmail.com>
2022-10-16 11:36:07 +0800

    maybe finish

--- app/Controller/registerMemberController.php
+++ app/Controller/registerMemberController.php
@@ -17,16 +17,29 @@ class registerMemberController
     public function storeMember()
     {
         $request = $_REQUEST;
-        if (!empty($request['first_name']) && !empty($request['last_name']) && !empty($request['student_id']) && !empty($request['class']) && !empty($request['friend']) && !empty($request['platform'])) {
-            $group_member = $this->generateGroupMemberService->logic($request['student_id'], $request['class'], $request['friend'], $request['platform']);
-            var_dump($group_member);
-            die;
-            if (!$group_member) {
+        if (!empty($request['first_name']) && !empty($request['last_name']) && !empty($request['student_id']) && !empty($request['class']) && !empty($request['day']) && !empty($request['time']) && !empty($request['platform'])) {
+            $group_member = $this->generateGroupMemberService->logic(
+                $request['student_id'],
+                $request['class'],
+                $request['friend'],
+                $request['platform'],
+                $request['day'],
+            );
+            if ($group_member == 400) {
                 $_SESSION['notification'] = 'Student Id Already Used, Please Check Your Input';
                 return header("Location: member.php");
+            } else if ($group_member == 401) {
+                $_SESSION['notification'] = 'Friend Not Found, Choose Other';
+                return header("Location: member.php");
+            } else if ($group_member == 402) {
+                $_SESSION['notification'] = 'All Group In This Workspace Is Full';
+                return header("Location: member.php");
             }
+            // store data here, we get group member
+            var_dump($group_member, $request);
+            die;
         } else {
-            $_SESSION['notification'] = 'All Field Is Required, Please Input All Data';
+            $_SESSION['notification'] = 'Please fill in the required data';
             return header("Location: member.php");
         }
     }
--- user/member.php
+++ user/member.php
@@ -29,7 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <?php require_once 'components/_navbar.php' ?>
     <div class="container m-custom-1">
         <div class="row">
-            <div class="col-12 col-md-5">
+            <div class="col-12 col-md-12">
                 <?php
                 if (isset($_SESSION['notification']) && $_SESSION['notification'] !== "") {
                     $element = '';
@@ -46,122 +46,82 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     <div class="card-header">
                         Form Add Data
                     </div>
-                    <div class="card-body">
-                        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
-                            <div class="mb-3">
-                                <label for="first_name" class="form-label">First Name</label>
-                                <input type="text" name="first_name" class="form-control transform-custom-1" id="first_name">
-                            </div>
-                            <div class="mb-3">
-                                <label for="last_name" class="form-label">Last Name</label>
-                                <input type="text" name="last_name" class="form-control transform-custom-1" id="last_name">
-                            </div>
-                            <div class="mb-3">
-                                <label for="student_id" class="form-label">Student Id</label>
-                                <input type="text" name="student_id" class="form-control" id="student_id">
-                            </div>
-                            <div class="mb-3">
-                                <label for="class" class="form-label">Your Class</label>
-                                <select class="form-select" id="class" onchange="selectClass(this.value)" name="class" aria-label="Default select example">
-                                    <option value=""> -- Select Class --</option>
-                                    <?php
-                                    $file_to_read = fopen("../database/General/all_class.csv", "r");
-                                    if ($file_to_read !== FALSE) {
-                                        while (($data = fgetcsv($file_to_read)) !== FALSE) {
-                                            foreach ($data as $i) {
-                                                echo '<option value="' . $data[0] . '">' . $data[1] . '</option>';
-                                                break;
+                    <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
+                        <div class="card-body row">
+                            <div class="col-12 col-md-6">
+                                <div class="mb-3">
+                                    <label for="first_name" class="form-label">First Name</label>
+                                    <input type="text" name="first_name" class="form-control transform-custom-1" id="first_name">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="last_name" class="form-label">Last Name</label>
+                                    <input type="text" name="last_name" class="form-control transform-custom-1" id="last_name">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="student_id" class="form-label">Student Id</label>
+                                    <input type="text" name="student_id" class="form-control" id="student_id">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="class" class="form-label">Your Workspace</label>
+                                    <select class="form-select" id="class" onchange="selectClass(this.value)" name="class" aria-label="Default select example">
+                                        <option value=""> -- Select Workspace --</option>
+                                        <?php
+                                        $file_to_read = fopen("../database/General/all_class.csv", "r");
+                                        if ($file_to_read !== FALSE) {
+                                            while (($data = fgetcsv($file_to_read)) !== FALSE) {
+                                                foreach ($data as $i) {
+                                                    echo '<option value="' . $data[0] . '">' . $data[1] . '</option>';
+                                                    break;
+                                                }
                                             }
+                                            fclose($file_to_read);
                                         }
-                                        fclose($file_to_read);
-                                    }
-                                    ?>
+                                        ?>
 
-                                </select>
-                            </div>
-                            <div class="mb-3">
-                                <label for="friend" class="form-label">Your Friend In Selected Class</label>
-                                <select class="form-select" id="friend" name="friend" aria-label="Default select example">
+                                    </select>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="friend" class="form-label">Your Friend In Selected Class (Optional)</label>
+                                    <select class="form-select" id="friend" name="friend" aria-label="Default select example">
 
-                                </select>
+                                    </select>
+                                </div>
                             </div>
-                            <div class="mb-3">
-                                <label for="platform" class="form-label">You Preffered Platform</label>
-                                <select class="form-select" name="platform" id="platform" aria-label="Default select example">
-                                    <option value=""> -- Select Platform --</option>
-                                    <option value="1">Website</option>
-                                    <option value="2">Desktop</option>
-                                    <option value="3">Mobile</option>
-                                </select>
+                            <div class="col-12 col-md-6">
+                                <div class="mb-3">
+                                    <label for="platform" class="form-label">You Preffered Platform</label>
+                                    <select class="form-select" name="platform" id="platform" aria-label="Default select example">
+                                        <option value=""> -- Select Platform --</option>
+                                        <option value="website">Website</option>
+                                        <option value="desktop">Desktop</option>
+                                        <option value="mobile">Mobile</option>
+                                    </select>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="day" class="form-label">Work Day</label>
+                                    <select class="form-select" name="day" id="day" aria-label="Default select example">
+                                        <option value=""> -- Select Day --</option>
+                                        <option value="monday">Monday</option>
+                                        <option value="tuesday">Tuesday</option>
+                                        <option value="wednesday">Wednesday</option>
+                                        <option value="thursday">Thursday</option>
+                                        <option value="friday">Friday</option>
+                                        <option value="saturday">Saturday</option>
+                                        <option value="sunday">Sunday</option>
+                                    </select>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="time" class="form-label">Working Time</label>
+                                    <select class="form-select" name="time" id="time" aria-label="Default select example">
+                                        <option value=""> -- Select Time --</option>
+                                        <option value="night">At Night</option>
+                                        <option value="day">At Day</option>
+                                    </select>
+                                </div>
                             </div>
                             <button class="btn bg-custom-1 text-custom-1 col-12">Submit Data</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-md-7">
-                <div class="card">
-                    <div class="card-header">
-                        All Data
-                    </div>
-                    <div class="card-body">
-                        <?php
-                        $file_to_read = fopen("../database/General/all_class.csv", "r");
-                        if ($file_to_read !== FALSE) {
-                            while (($data = fgetcsv($file_to_read)) !== FALSE) {
-                                foreach ($data as $i) {
-                                    $element = '';
-                                    $element .=  '<div class="m-custom-2">';
-                                    $element .= '<h5 class="text-custom-2">' . $data[0] . " - " . $data[1] . '</h5>';
-                                    $element .= '<table class="table table-striped">';
-                                    $element .= '<thead class="bg-custom-1 text-custom-1">';
-                                    $element .= '<tr>';
-                                    $element .= '<th scope="col" class="size-custom-1">#</th>';
-                                    $element .= '<th scope="col" class="size-custom-1">Student Id</th>';
-                                    $element .= '<th scope="col" class="size-custom-1">First Name</th>';
-                                    $element .= '<th scope="col" class="size-custom-1">Last Name</th>';
-                                    $element .= '<th scope="col" class="size-custom-1">Class Name</th>';
-                                    $element .= '<th scope="col" class="size-custom-1">Group</th>';
-                                    $element .= '<th scope="col" class="size-custom-1">Preffered Platform</th>';
-                                    $element .= '<th scope="col" class="size-custom-1">Created At</th>';
-                                    $element .= '</tr>';
-                                    $element .= '</thead>';
-                                    $element .= '<tbody>';
-
-                                    $filename = "../database/Uploaded/" . $data[2] . "";
-                                    if (!file_exists($filename)) {
-                                        $element .= '<tr>';
-                                        $element .= '<td colspan="8" class="text-center">Data Not Found</td>';
-                                        $element .= '</tr>';
-                                    } else {
-                                        $member_file_read = fopen($filename, "r");
-                                        if ($member_file_read !== FALSE) {
-                                            while (($member = fgetcsv($member_file_read)) !== FALSE) {
-                                                $element .= '<tr>';
-                                                for ($j = 0; $j < count($member); $j++) {
-                                                    if ($j == 7) {
-                                                        $element .= '<td class="size-custom-1">' . $date->format($member[$j]) . '</td>';
-                                                    } else {
-                                                        $element .= '<td class="size-custom-1">' . $member[$j] . '</td>';
-                                                    }
-                                                }
-                                                $element .= '</tr>';
-                                            }
-                                            fclose($member_file_read);
-                                        }
-                                    }
-                                    $element .= '</tbody>';
-                                    $element .= '</table>';
-                                    $element .= '</div>';
-                                    echo $element;
-                                    break;
-                                }
-                            }
-                            fclose($file_to_read);
-                        }
-                        ?>
-
-                    </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
